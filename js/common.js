9e5ef56b6410d7d1d9ff6c0b9bb1f45f4badb5a1
rogi39 <igor93osetia@yandex.ru>
2019-02-07 00:31:18 +0300

    add item in portfolio

--- js/common.js
+++ js/common.js
@@ -2,4 +2,14 @@ $(function() {
 
 	$('.parallax-window').parallax({imageSrc: '../img/parallax.jpg'});
 
+
+$('.single-item').slick({
+	adaptiveHeight: true,
+	dots: true,
+	infinite: false,
+	fade: true,
+	
+});
+
+
 });
